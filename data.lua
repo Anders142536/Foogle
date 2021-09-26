a9733794f9046f3f32a6abdea640168678e462e0
Anders <dom.galbavy@gmail.com>
2021-09-27 00:54:53 +0200

    added data structure added open and close shortcuts

---
+++ data.lua
@@ -0,0 +1,15 @@
+--require("prototypes.sprites")
+--require("prototypes.styles")
+
+data:extend{
+    {
+        type = "custom-input",
+        name = "FOO-open-gui",
+        key_sequence = "CONTROL + F"
+    },
+    {
+        type = "custom-input",
+        name = "FOO-close-gui",
+        key_sequence = "ESCAPE"
+    }
+}
