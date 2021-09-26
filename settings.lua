87be17ac72bf9480e21ea6498f7b63338359ed36
Anders <dom.galbavy@gmail.com>
2021-09-27 00:55:04 +0200

    added debug setting

---
+++ settings.lua
@@ -0,0 +1,10 @@
+-- settings are done in ingame ui
+data:extend(
+{
+    {
+        type = "bool-setting",
+        name = "FOO-enable-debug",
+        setting_type = "runtime-global",
+        default_value = false
+    }
+})
