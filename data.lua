fff72e67b6e244d895dbd0d4c0702d863e5c0911
Anders <dom.galbavy@gmail.com>
2021-09-29 01:05:28 +0200

    removed escape custom input, as the opened attribute is a cleaner solution

--- data.lua
+++ data.lua
@@ -6,10 +6,5 @@ data:extend{
         type = "custom-input",
         name = "FOO-open-gui",
         key_sequence = "CONTROL + F"
-    },
-    {
-        type = "custom-input",
-        name = "FOO-close-gui",
-        key_sequence = "ESCAPE"
     }
 }
